00e4f245a70a790b311ed444bb0f23eb58cf67e9
joetayactac <jt.appsheet@gmail.com>
2026-08-04 09:30:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,9 @@
   /* ---------------- State ---------------- */
 
   let columns = [
-    { header: "Vehicle No", column: "VEHICLE NO", sample: "382231" },
-    { header: "Vehicle Name", column: "VEHICLE NAME", sample: "Mercedes S450" },
-    { header: "Reg Month", column: "REG MONTH", sample: "November" }
+    { header: "Header Name 1", column: "Appsheet Column Name 1", sample: "Text 1" },
+    { header: "Header Name 2", column: "Appsheet Column Name 2", sample: "Text 2" },
+    { header: "Header Name 3", column: "Appsheet Column Name 3", sample: "Text 3" }
   ];
   let nextId = columns.length;
   columns = columns.map((c, i) => ({ id: i, ...c }));
